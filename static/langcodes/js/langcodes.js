263b7952f241893cd921f7dd17334dcf06943a99
Rohit Negi <rohit25.negi@gmail.com>
2018-09-03 10:13:52 +0530

    enclosing langcode code in define

--- static/langcodes/js/langcodes.js
+++ static/langcodes/js/langcodes.js
@@ -1,41 +1,47 @@
-jQuery.prototype.langcodes = function() {
-    return this.select2({
-        minimumInputLength: 0,
-        delay: 100,
-        initSelection: function(element, callback) {
-            callback({
-                id: element.val(),
-                text: element.val(),
-            });
-        },
-        // Allow manually entered text in drop down, which is not supported by legacy select2.
-        createSearchChoice: function(term, data) {
-            if (!_.find(data, function(d) { return d.text === term; })) {
-                return {
-                    id: term,
-                    text: term,
-                };
-            }
-        },
-        ajax: {
-            url: "/langcodes/langs.json",
-            data: function(term) {
-                return {
-                    term: term,
-                };
+hqDefine('langcodes/js/langcodes',[
+    'jquery',
+    'select2-3.4.5-legacy/select2.min',
+],function($){
+    $.prototype.langcodes = function() {
+        return this.select2({
+            minimumInputLength: 0,
+            delay: 100,
+            initSelection: function(element, callback) {
+                callback({
+                    id: element.val(),
+                    text: element.val(),
+                });
             },
-            dataType: 'json',
-            results: function(data) {
-                return {
-                    results: $.map(data, function(item) {
-                        return {
-                            text: item.code + " (" + item.name + ") ",
-                            id: item.code,
-                        };
-                    }),
-                };
+            // Allow manually entered text in drop down, which is not supported by legacy select2.
+            createSearchChoice: function(term, data) {
+                if (!_.find(data, function(d) { return d.text === term; })) {
+                    return {
+                        id: term,
+                        text: term,
+                    };
+                }
             },
-            cache: true,
-        },
-    });
-}
+            ajax: {
+                url: "/langcodes/langs.json",
+                data: function(term) {
+                    return {
+                        term: term,
+                    };
+                },
+                dataType: 'json',
+                results: function(data) {
+                    return {
+                        results: $.map(data, function(item) {
+                            return {
+                                text: item.code + " (" + item.name + ") ",
+                                id: item.code,
+                            };
+                        }),
+                    };
+                },
+                cache: true,
+            },
+        });
+    };
+
+});
